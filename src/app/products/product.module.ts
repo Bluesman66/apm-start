ef3a829f8eb54c3380a10ff9828091a02d141d73
Bluesman66 <alex.paclin@gmail.com>
2019-01-26 22:38:16 +0500

    02 Setting up for Feature Routing

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductListComponent } from './product-list.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent }
+    ])
   ],
   declarations: [
     ProductListComponent,
