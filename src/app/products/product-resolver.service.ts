93e9c3845f8fa49cd32c59c7002a4410ee40ca4f
alex.paclin <alex.paclin@gmail.com>
2019-01-28 13:48:45 +0500

    04 Building a Route Resolver Service

---
+++ src/app/products/product-resolver.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Injectable()
+export class ProductResolver implements Resolve<Product> {
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+    const id = route.paramMap.get('id');
+    if (isNaN(+id)) {
+      console.log(`Product id was not a number ${id}`);
+      this.router.navigate(['/products']);
+      return of(null);
+    }
+    return this.productService.getProduct(+id)
+      .pipe(
+        map(product => {
+          if (product) {
+            return product;
+          }
+          console.log(`Product was not found ${id}`);
+          this.router.navigate(['/products']);
+          return null;
+        }),
+        catchError(error => {
+          console.error(`Retrieval error: ${error}`);
+          this.router.navigate(['/products']);
+          return of(null);
+        })
+      );
+  }
+}
