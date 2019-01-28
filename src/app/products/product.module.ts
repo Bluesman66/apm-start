d7d11abdda5b0646e87346057b5449d2f4fc9962
alex.paclin <alex.paclin@gmail.com>
2019-01-28 13:57:24 +0500

    05 Adding a Resolver to a Route Configuration

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -5,14 +5,15 @@ import { SharedModule } from '../shared/shared.module';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductListComponent } from './product-list.component';
+import { ProductResolver } from './product-resolver.service';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent }
+      { path: 'products/:id', component: ProductDetailComponent, resolve: { product: ProductResolver } },
+      { path: 'products/:id/edit', component: ProductEditComponent, resolve: { product: ProductResolver } }
     ])
   ],
   declarations: [
